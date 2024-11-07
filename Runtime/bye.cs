7db9b168a7a56f1d7e862782a3f870fb65c076d4
SanthanavelR <santhanavel@neurindustries.com>
2024-11-07 14:24:09 +0530

    Bye

--- Runtime/bye.cs
+++ Runtime/bye.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class bye : MonoBehaviour
+namespace Neur.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class bye : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
