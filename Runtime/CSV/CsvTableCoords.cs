e3a9782d6c0d642b7e82d6e4246b189b515ab393
SanthanavelR <santhanavel@neurindustries.com>
2024-11-07 14:43:29 +0530

    New

---
+++ Runtime/CSV/CsvTableCoords.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ /// <summary>
+    /// csv表格坐标（行，列）
+    /// </summary>
+    public struct CsvTableCoords
+    {
+        public int row;
+        public int column;
+
+        public CsvTableCoords(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public void Set(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+    }
---
+++ Runtime/Data Scripts/DataManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Neur.UI
+{
+    public class DataManager : MonoBehaviour
+    {
+        public class StaticVariables
+        {
+            //***********UI Automation****************
+
+            public static int STEP_COUNT = 0;
+
+            public static string HIGHLIGHT_COLOR = "yellow";
+            public static string CF_OBJECT_NAME = string.Empty;
+            public static bool IS_CF_HOLDED = default;
+            public static bool Is_BG_MUTED = default;
+            public static bool Is_SE_MUTED = default;
+
+            //To parse CSV data all column value for particular row
+            public static string COLUMN_00 = string.Empty;
+            public static string COLUMN_01 = string.Empty;
+            public static string COLUMN_02 = string.Empty;
+            public static string COLUMN_03 = string.Empty;
+            public static string COLUMN_04 = string.Empty;
+            public static string COLUMN_05 = string.Empty;
+            public static string COLUMN_06 = string.Empty;
+            public static string COLUMN_07 = string.Empty;
+            public static string COLUMN_08 = string.Empty;
+            public static string COLUMN_09 = string.Empty;
+            public static string COLUMN_10 = string.Empty;
+            public static string COLUMN_11 = string.Empty;
+            public static string COLUMN_12 = string.Empty;
+        };
+    }
+
+}
