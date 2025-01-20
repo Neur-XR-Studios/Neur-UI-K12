5b974e668cc483719be5ec016a8d0db607a2d1dc
SanthanavelR <santhanavel@neurindustries.com>
2025-01-20 19:55:36 +0530

    New Version

---
+++ Runtime/Controllers Scripts/VoiceoverControllerScript.cs
@@ -0,0 +1,105 @@
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+namespace K12.UI
+{
+    public class VoiceoverControllerScript : MonoBehaviour
+    {
+        [Space(10)]
+        [Header("----------------Voiceover UI REFERENCES--------------------")]
+        [Space(20)]
+        public Button Mute_Button;
+        public static bool isActivity = true;
+        public bool IsMuted = false;
+        public Sprite mute, unmute;
+        [HideInInspector]
+        public static AudioSource VoiceOverAudioSource;
+        public AudioSource[] BG_AudioSource;
+
+
+        #region Initialization
+        private void Start()
+        {
+            Initialisation();
+        }
+        void Initialisation()
+        {
+            Mute_Button.onClick.AddListener(() => MuteButtonClickHandler(Mute_Button));
+            VoiceOverAudioSource = gameObject.GetComponent<AudioSource>();
+        }
+        #endregion
+
+        void PlaySubStepVoiceOver()
+        {
+            string audioClipName = DataManager.StaticVariables.COLUMN_05;
+            AudioClip clip = Resources.Load<AudioClip>($"Audios/{audioClipName}");
+            VoiceOverAudioSource.clip = clip;
+            VoiceOverAudioSource.Play();
+        }
+        void PlayPromtVoiceOver()
+        {
+            string audioClipName = DataManager.StaticVariables.COLUMN_06;
+            AudioClip clip = Resources.Load<AudioClip>($"Audios/{audioClipName}");
+            if (clip != null)
+            {
+                VoiceOverAudioSource.clip = clip;
+                VoiceOverAudioSource.Play();
+            }
+            else
+            {
+                Debug.Log("Audioclip not found!");
+            }
+        }
+
+        public void MuteButtonClickHandler(Button muteButton)
+        {
+
+            if (!IsMuted)
+            {
+                Debug.Log(PlayerPrefs.GetInt("BackgroundAudioMuteState"));
+                muteButton.image.sprite = mute;
+                if (PlayerPrefs.GetInt("BackgroundAudioMuteState") == 1)
+                {
+                    BG_AudioSource[0].mute = true;
+                }
+                if (PlayerPrefs.GetInt("SoundEffectMuteState") == 1)
+                {
+                    BG_AudioSource[1].mute = true;
+                }
+                VoiceOverAudioSource.mute = true;
+            }
+            else
+            {
+                muteButton.image.sprite = unmute;
+                if (PlayerPrefs.GetInt("BackgroundAudioMuteState") == 1)
+                {
+                    BG_AudioSource[0].mute = false;
+                }
+                if (PlayerPrefs.GetInt("SoundEffectMuteState") == 1)
+                {
+                    BG_AudioSource[1].mute = false;
+                }
+                VoiceOverAudioSource.mute = false;
+
+            }
+            IsMuted = !IsMuted;
+          
+        }
+
+        #region Event subscription
+        //subribed on VoiceOver event
+        private void OnEnable()
+        {
+            EventManager.AddHandler(EVENTS.STEP, PlaySubStepVoiceOver);
+            EventManager.AddHandler(EVENTS.PROMPT, PlayPromtVoiceOver);
+        }
+        //unsubribed from SPAW event
+        private void OnDisable()
+        {
+            EventManager.RemoveHandler(EVENTS.STEP, PlaySubStepVoiceOver);
+            EventManager.RemoveHandler(EVENTS.PROMPT, PlayPromtVoiceOver);
+        }
+        #endregion
+    }
+
+}
---
+++ Runtime/Easy Csv/CsvTableCoords.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace K12.UI
+{
+    public struct CsvTableCoords
+    {
+        public int row;
+        public int column;
+
+        public CsvTableCoords(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public void Set(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+    }
+
+}
